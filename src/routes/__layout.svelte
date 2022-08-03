97c7a8ba943bdaabe24ac5e7d745c3800c1fcc35
jeel2331 <jeelrajodiyajeel@gmail.com>
2022-08-03 19:23:56 +0530

    chanes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,28 +1,29 @@
 <script>
-    import "../app.css"
-    import DownloadIcon from "$lib/assets/icons8-download-24.png"
+	import '../app.css';
+	import DownloadIcon from '$lib/assets/icons8-download-24.png';
 </script>
+
 <svelte:head>
-    <title>Svelte To Do</title>
+	<title>Svelte To Do</title>
 </svelte:head>
 
 <main>
-<div class="flex justify-center">
-    <div class="navbar bg-neutral flex justify-center rounded-xl max-w-xl">
-        <div >
-          <a href="/" class="btn btn-ghost normal-case text-xl">Svelte To Do</a>
-        </div>
-       
-      </div>
-    </div>
-    <slot></slot>
+	<div class="flex justify-center">
+		<div class="navbar bg-neutral flex justify-center rounded-xl max-w-xl">
+			<div>
+				<a href="/" class="btn btn-ghost normal-case text-xl">Svelte To Do</a>
+			</div>
+		</div>
+	</div>
+	<slot />
 </main>
+
 <style global>
-    body{
-        padding:15px;
-        height: 100%;
-    }
-    html{
-        height: 100%;
-    }
+	body {
+		padding: 15px;
+		height: 100%;
+	}
+	html {
+		height: 100%;
+	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,60 +1,49 @@
 <script>
-    import SvelteIcon from "$lib/assets/SvelteIcon.svelte";
-    import WindowsLogo from "$lib/assets/Windows.svelte";
-    import AndroidLogo from "$lib/assets/Android.svelte";
+	import SvelteIcon from '$lib/assets/SvelteIcon.svelte';
+	import WindowsLogo from '$lib/assets/Windows.svelte';
+	import AndroidLogo from '$lib/assets/Android.svelte';
 </script>
 
 <main class="flex justify-center align-middle flex-col ">
-    
-    <h1 class="my-10">
-        To Do app made with 
-        <svelte:component  data-tip="svelte" this={SvelteIcon} />
-    </h1>
-<div class="card bg-neutral w-full m-5 p-10 max-w-xl">
-    <h2 class="card-title justify-center p-5">
-        <svelte:component this={AndroidLogo} />
-        Download for Android
-    </h2>
+	<h1 class="my-10">
+		To Do app made with
+		<svelte:component this={SvelteIcon} data-tip="svelte" />
+	</h1>
+	<div class="card bg-neutral w-full m-5 p-10 max-w-xl">
+		<h2 class="card-title justify-center p-5">
+			<svelte:component this={AndroidLogo} />
+			Download for Android
+		</h2>
 
-<button class=" btn btn-primary card-action">
-    Download .apk
-</button>
+		<button class=" btn btn-primary card-action"> Download .apk </button>
+	</div>
 
-</div>
+	<div class="card bg-neutral m-5 w-full p-10 max-w-xl">
+		<h2 class="card-title justify-center p-5">
+			<svelte:component this={WindowsLogo} />
+			Download for Windows <b>64bit</b>
+		</h2>
 
-<div class="card bg-neutral m-5 w-full p-10 max-w-xl">
-    <h2 class="card-title justify-center p-5">
-        <svelte:component this={WindowsLogo} />
-        Download for Windows <b>64bit</b>
-    </h2>
+		<button class=" btn btn-primary card-action"> Download .exe installer </button>
+	</div>
 
-<button class=" btn btn-primary card-action">
-    Download .exe installer
-</button>
+	<div class="card bg-neutral m-5 w-full p-10 max-w-xl">
+		<h2 class="card-title justify-center p-5">
+			<svelte:component this={WindowsLogo} />
+			Download for Windows <b>32bit</b>
+		</h2>
 
-</div>
-
-<div class="card bg-neutral m-5 w-full p-10 max-w-xl">
-    <h2 class="card-title justify-center p-5">
-        <svelte:component this={WindowsLogo} />
-        Download for Windows <b>32bit</b> 
-    </h2>
-
-<button class=" btn btn-primary card-action">
-    Download .exe installer 
-</button>
-
-</div>
-
+		<button class=" btn btn-primary card-action"> Download .exe installer </button>
+	</div>
 </main>
 
 <style>
-    main{
-        margin:15px 0 ;
-        align-items: center;
-        height: 100%;
-    }
-    h1{
-        font-size: 2rem;
-    }
+	main {
+		margin: 15px 0;
+		align-items: center;
+		height: 100%;
+	}
+	h1 {
+		font-size: 2rem;
+	}
 </style>

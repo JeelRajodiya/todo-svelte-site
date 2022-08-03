6acb667d46602def0c10eaa0d32f5f4ec418d88c
jeel2331 <jeelrajodiyajeel@gmail.com>
2022-08-03 19:23:29 +0530

    changes

---
+++ src/routes/login.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+</svelte:head>
+<div id="g_id_onload"
+data-login_uri="http://localhost:5000/login"
+
+data-client_id="939265980834-pkb13au0c1hj7aiu32dmui1bthkqho9l.apps.googleusercontent.com"></div>
+     

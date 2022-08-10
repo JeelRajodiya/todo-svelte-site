e95a64d1ac7bd637f7c76c965cce4ae6c7885a32
jeel2331 <jeelrajodiyajeel@gmail.com>
2022-08-10 19:33:59 +0530

    tasks.clear

---
+++ src/routes/api/tasklists/[tasklistID]/clear.ts
@@ -0,0 +1,35 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import MongoDB from '$lib/database';
+import etag from 'etag';
+
+export async function POST(event: RequestEvent) {
+	const taskListID: string = event.params.taskListID;
+
+	const session: string = event.request.headers.get('Authorization') as string;
+	if (session == null) {
+		return {
+			status: 400,
+			body: {
+				message: 'make sure authorization header  is present'
+			}
+		};
+	}
+	const db = MongoDB;
+	const user = await db.users.findOne({ sessions: session });
+	if (user === null) {
+		return {
+			status: 400,
+			body: {
+				message: 'Invalid session ID'
+			}
+		};
+	}
+	const userID = user.id;
+	const deletedCount = await db.tasks.deleteMany({ taskListID, userID, isCompleted: true });
+	return {
+		status: 200,
+		body: {
+			message: 'deleted ' + deletedCount.deletedCount + ' completed tasks'
+		}
+	};
+}

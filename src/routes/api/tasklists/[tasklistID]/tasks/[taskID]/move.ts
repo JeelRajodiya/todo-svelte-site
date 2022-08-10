3a957da256720366ac90c9427ed07eae23128519
jeel2331 <jeelrajodiyajeel@gmail.com>
2022-08-10 21:33:01 +0530

    move

--- src/routes/api/tasklists/[tasklistID]/tasks/[taskID]/move.ts
+++ src/routes/api/tasklists/[tasklistID]/tasks/[taskID]/move.ts
@@ -1,5 +1,6 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import MongoDB from '$lib/database';
+import { stringToBool } from '$lib/util/functions';
 
 export async function POST(event: RequestEvent) {
 	const taskListID: string = event.params.taskListID;
@@ -25,7 +26,7 @@ export async function POST(event: RequestEvent) {
 		};
 	}
 	const userID = user.id;
-	const tasks = await db.tasks.find({ userID, taskListID }).sort({ position: 1 }).toArray();
+	let tasks = await db.tasks.find({ userID, taskListID }).sort({ position: 1 }).toArray();
 
 	if (tasks === null) {
 		return {
@@ -35,10 +36,37 @@ export async function POST(event: RequestEvent) {
 			}
 		};
 	}
+	const body = await event.request.json();
+	const isChild = stringToBool(body.isChild, false);
+	const newPosition = body.newPosition;
+	// getting the currentPosition of the task before it gets deleted
+	const taskPosition = tasks.filter((i) => i.id == taskID)[0].position;
+	if (newPosition == undefined) {
+		return {
+			status: 400,
+			body: { message: 'provide newPosition arguments in body' }
+		};
+	}
 
-	console.log(tasks);
+	// removing the current task from list
+	tasks = tasks.filter((i) => i.id != taskID);
+	// moving a task
+	// task after and at the newPosition and before the currentPosition will have increment of 1
+	// rest remains same
 
+	let p; // position
+	for (let i = 0; i < tasks.length; i++) {
+		p = tasks[i].position;
+
+		if (p >= newPosition && p <= taskPosition) {
+			tasks[i].position = p + 1;
+		}
+	}
+	const updateResult = await db.tasks.updateMany({ userID, taskListID }, tasks);
 	return {
-		status: 200
+		status: 200,
+		body: {
+			message: `updated ${updateResult.modifiedCount} tasks`
+		}
 	};
 }

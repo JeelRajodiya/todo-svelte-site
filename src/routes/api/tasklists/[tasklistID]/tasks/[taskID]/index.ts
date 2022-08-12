1bffbeb8cce52c3aff3086872100c697133d6e8a
jeel2331 <jeelrajodiyajeel@gmail.com>
2022-08-12 20:28:00 +0530

    upate task

--- src/routes/api/tasklists/[tasklistID]/tasks/[taskID]/index.ts
+++ src/routes/api/tasklists/[tasklistID]/tasks/[taskID]/index.ts
@@ -1,6 +1,6 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import MongoDB from '$lib/database';
-
+import etag from 'etag';
 export async function DELETE(event: RequestEvent) {
 	const taskListID: string = event.params.taskListID;
 	const taskID: string = event.params.taskID;
@@ -34,5 +34,66 @@ export async function DELETE(event: RequestEvent) {
 	}
 	return {
 		status: 200
+	};
+}
+
+export async function PUT(event: RequestEvent) {
+	const taskListID: string = event.params.taskListID;
+	const taskID: string = event.params.taskID;
+	const body = await event.request.json();
+	if (body.updatedOn == undefined) {
+		body.updatedOn = new Date();
+	}
+	const session: string = event.request.headers.get('Authorization') as string;
+	if (session == null) {
+		return {
+			status: 400,
+			body: {
+				message: 'make sure authorization header  is present'
+			}
+		};
+	}
+	const db = MongoDB;
+	const user = await db.users.findOne({ sessions: session });
+	if (user === null) {
+		return {
+			status: 400,
+			body: {
+				message: 'can not find user'
+			}
+		};
+	}
+	const userID = user.id;
+
+	db.tasks.updateOne({ userID, taskListID, id: taskID }, { $set: body });
+	const taskDoc = await db.tasks.findOne({ userID, taskListID, id: taskID });
+	if (taskDoc === null) {
+		return {
+			status: 400,
+			body: { message: 'something went wrong ' }
+		};
+	}
+	db.tasks.updateOne(
+		{ userID, taskListID, id: taskID },
+		{
+			$set: {
+				etag: etag(
+					String(taskDoc.isCompleted) +
+						taskDoc?.links +
+						taskDoc?.due +
+						taskDoc?.notes +
+						taskDoc?.parent +
+						taskDoc.position +
+						taskDoc.updatedOn
+				)
+			}
+		}
+	);
+
+	return {
+		status: 200,
+		body: {
+			message: `modified `
+		}
 	};
 }

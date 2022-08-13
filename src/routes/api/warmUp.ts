0dbe70560949548ff7a887fe399680b0bfb2bdc1
jeel2331 <jeelrajodiyajeel@gmail.com>
2022-08-13 16:29:46 +0530

    added user session validation on warmUp

--- src/routes/api/warmUp.ts
+++ src/routes/api/warmUp.ts
@@ -1,6 +1,26 @@
 // just to warm up the server and database
 import MongoDB from '$lib/database';
-export async function GET() {
+import type { RequestEvent } from '@sveltejs/kit';
+export async function GET(event: RequestEvent) {
+	const session: string = event.request.headers.get('Authorization') as string;
+	if (session == null) {
+		return {
+			status: 400,
+			body: {
+				message: 'make sure authorization header  is present'
+			}
+		};
+	}
+	const db = MongoDB;
+	const user = await db.users.findOne({ sessions: session });
+	if (user === null) {
+		return {
+			status: 403,
+			body: {
+				message: 'can not find user. must be logged out'
+			}
+		};
+	}
 	return {
 		status: 200
 	};
